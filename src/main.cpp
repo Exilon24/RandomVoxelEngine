7018ab716144ceb278f932996e1648033873a0fe
Exilon <explunki@outlook.com>
2024-08-05 17:56:07 +0400

    changed camera keybinds

--- src/main.cpp
+++ src/main.cpp
@@ -241,9 +241,9 @@ void processInput(GLFWwindow* window)
         playerCam.position += playerCam.right * camSpeed;
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         playerCam.position -= playerCam.right * camSpeed;
-    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
         playerCam.position += playerCam.localUp * camSpeed;
-    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
         playerCam.position -= playerCam.localUp * camSpeed;
 
 }
